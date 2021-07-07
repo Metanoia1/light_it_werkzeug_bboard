2ea73e756b665e795ae7256352fd0c1f6fa22012
metanoia <meta3479@gmail.com>
2021-07-07 14:13:24 +0300

    deploy 1

---
+++ bboard.py
@@ -0,0 +1,79 @@
+"""A simple URL shortener using Werkzeug and redis."""
+import os
+
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import NotFound
+from werkzeug.routing import Map
+from werkzeug.routing import Rule
+from werkzeug.utils import redirect
+from werkzeug.wrappers import Request
+from werkzeug.wrappers import Response
+
+from db_settings import Announcement, Comment, Session
+
+
+session = Session()
+
+
+class BBoard(object):
+    def __init__(self):
+        template_path = os.path.join(os.path.dirname(__file__), "templates")
+        self.jinja_env = Environment(
+            loader=FileSystemLoader(template_path), autoescape=True
+        )
+        self.url_map = Map(
+            [
+                Rule("/", endpoint="index"),
+                Rule("/<add>", endpoint="add_announcement"),
+            ]
+        )
+
+    def render_template(self, template_name, **context):
+        t = self.jinja_env.get_template(template_name)
+        return Response(t.render(context), mimetype="text/html")
+
+    def dispatch_request(self, request):
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, f"on_{endpoint}")(request, **values)
+        except NotFound:
+            return self.error_404()
+        except HTTPException as e:
+            return e
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def on_index(self, request):
+        context = {"announcements": session.query(Announcement).all()}
+        return self.render_template("index.html", **context)
+
+    def on_add_announcement(self, request, add):
+        if request.method == "POST":
+            new_announcement = Announcement(
+                author=f"{request.values['author']}",
+                title=f"{request.values['title']}",
+                text=f"{request.values['text']}",
+            )
+            session.add(new_announcement)
+            session.commit()
+        context = {}
+        return self.render_template("add_announcement.html", **context)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+
+def application(environ, start_response):
+    app = BBoard()
+    return app(environ, start_response)
+
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    # run_simple("127.0.0.1", 5000, app, use_debugger=True, use_reloader=True)
---
+++ db_settings.py
@@ -0,0 +1,54 @@
+import os
+import psycopg2
+
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    Index,
+    Date,
+    DateTime,
+    Numeric,
+    BigInteger,
+    String,
+    ForeignKey,
+    Boolean,
+)
+
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+Base = declarative_base()
+
+
+class Announcement(Base):
+    __tablename__ = "announcements"
+
+    id = Column(Integer, primary_key=True)
+    author = Column(String(length=100))
+    title = Column(String(length=100))
+    text = Column(String(length=1000))
+    comment = relationship("Comment")
+
+    def __repr__(self):
+        return f"{self.title} ({self.author})"
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True)
+    author = Column(String(length=100))
+    text = Column(String(length=200))
+    announcement_id = Column(Integer, ForeignKey("announcements.id"))
+
+    def __repr__(self):
+        return f"{self.author}"
+
+
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
